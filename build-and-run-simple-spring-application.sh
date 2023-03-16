002749fb432aa89a011f9dc3cd16bc33b437a5bb
Samantha Tobias <SamanthaArnoldussen@gmail.com>
2023-03-16 20:52:08 +0100

    fix port not working

--- build-and-run-simple-spring-application.sh
+++ build-and-run-simple-spring-application.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Build the Maven project
-mvn package -Dspring.config.location=classpath:application.properties
+mvn package #-Dspring.config.location=classpath:application.properties
 
 # Build the Docker image
 docker build -t my-docker-java-app .
 
 # Run the Docker container
-docker run my-docker-java-app
+docker run -p 9000:9000 my-docker-java-app
